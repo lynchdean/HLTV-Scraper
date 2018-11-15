2651a787524c1b3d1fedbd2f6c4ac320c813011e
Riain Condon <riain.condon4@mail.dcu.ie>
2018-11-15 15:41:38 +0000

    Update scraper

--- scraper.py
+++ scraper.py
@@ -15,20 +15,49 @@ matches = [a.get('href') for a in soup.find_all('a', class_='a-reset') if a.get(
 match_page = requests.get(base_url + matches[0])
 match_soup = BeautifulSoup(match_page.text, 'lxml')
 
-maps_raw = match_soup.find_all('div', class_='mapholder') #.find('a').get('href')
-for map in maps_raw:
-    map_path = map.find('a').get('href')
-    detailed_page = requests.get(base_url + map_path)
-    detailed_soup = BeautifulSoup(detailed_page.text, 'lxml')
+with open("stats.txt", "w") as f:
+    f.write('match_id, team_name, player_name, kills, assists, flash_assists, deaths, kd_ratio, kd_diff, adr, fk_dif, rating\n')
 
-    stats_tables = detailed_soup.find_all('table', class_='stats-table')
-    print(stats_tables)
+i = 0
+while i < len(matches):
+    match_page = requests.get(base_url + matches[i])
+    match_soup = BeautifulSoup(match_page.text, 'lxml')
 
-# detailed_page = requests.get(base_url + detailed_path)
-# detailed_path = match_soup.find('div', class_='stats-detailed-stats').find('a').get('href')
-# time.sleep(1)
-# detailed_soup = BeautifulSoup(detailed_page.text, 'lxml')
-# print(detailed_soup)
-#
-# test = detailed_soup.find('div', class_='headline')
-# print(test)
+    maps_raw = match_soup.find_all('div', class_='mapholder')
+    for map in maps_raw:
+        map_path = map.find('a').get('href')
+        match_id = map_path.split('/')[-2]
+        detailed_page = requests.get(base_url + map_path)
+        detailed_soup = BeautifulSoup(detailed_page.text, 'lxml')
+
+        stats_tables = detailed_soup.find_all('table', class_='stats-table')
+        for table in stats_tables:
+            team_name = table.find('th', class_='st-teamname').text
+
+            table_body = table.find('tbody')
+            rows = table_body.find_all('tr')
+            for row in rows:
+                player_name = row.find('td', class_='st-player').text
+
+                kills_hs = (row.find('td', class_='st-kills').text).split(" ")
+                kills = kills_hs[0]
+
+                headshots = kills_hs[1].strip("(").strip(")")
+
+                assists_fl = row.find('td', class_='st-assists').text.split(" ")
+                assists = assists_fl[0]
+                flash_assists = assists_fl[1].strip("(").strip(")")
+
+                deaths = row.find('td', class_='st-deaths').text
+                kd_ratio = float(row.find('td', class_='st-kdratio').text.strip("%")) / 100
+                kd_diff = row.find('td', class_='st-kddiff').text.strip("+")
+                adr = row.find('td', class_='st-adr').text
+                fk_dif = row.find('td', class_='st-kddiff').text.strip("+")
+                rating = row.find('td', class_='st-rating').text
+                with open("stats.txt", "a") as f:
+                    f.write("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}\n".format(match_id, team_name, player_name, kills, assists, flash_assists, deaths, kd_ratio, kd_diff, adr, fk_dif, rating))
+                # print(match_id, team_name, player_name, kills, assists, flash_assists, deaths, kd_ratio, kd_diff, adr, fk_dif, rating)
+    i += 1
+
+
+
